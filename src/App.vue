044c7dc43c54e8934c1a7bde4175d1fa520fd0a9
Levon Mkrtchian <>
2018-11-18 21:54:30 +0100

    no message

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,18 @@
 </template>
 
 <script>
+import Vue from "vue";
+import VueGAPI from "vue-gapi";
 import Time from './components/Time.vue'
+
+const apiConfig = {
+  apiKey: "AIzaSyC9hnF1r533j1gZsfXIJHmtT7MbsSd71TY",
+  clientId: "70186310433-j4n0th47upd6vfs07p0edpldeg65iqpq.apps.googleusercontent.com",
+  discoveryDocs: ["https://sheets.googleapis.com/$discovery/rest?version=v4"],
+  scope: "https://www.googleapis.com/auth/spreadsheets.readonly"
+};
+
+Vue.use(VueGAPI, apiConfig);
 
 export default {
   name: 'app',
--- src/components/Time.vue
+++ src/components/Time.vue
@@ -1,16 +1,44 @@
 <template>
   <div>
-    test
+    <button v-show="!isAuthenticated" v-on:click="login()">Authorize</button>
+    <button v-show="isAuthenticated" v-on:click="logout()">Sign Out</button>
+    
   </div>
 </template>
 
 <script>
+
+
 export default {
-  name: 'Time',
-  props: {
-   
+  name: "Time",
+  props: {},
+  data: function() {
+    return {
+      isAuthenticated: false
+    }
+  },
+  computed: {
+    // isSignedIn () {
+    //   return this.$isAuthenticated()
+    // }
+  },
+  methods: {
+    login () {
+      if (this.$isAuthenticated() !== true) {
+        this.$login();
+        this.isAuthenticated = this.$isAuthenticated();
+      }
+    },
+    logout () {
+      this.$logout();
+      this.isAuthenticated = this.$isAuthenticated();
+    }
+  },
+  created: function() {
+    this.isAuthenticated = this.$isAuthenticated();
   }
-}
+};
+
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
